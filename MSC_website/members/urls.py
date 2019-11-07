d817ce69602d543eb04970b2bbff07f6ad88a7f4
parthrr510 <42448056+parthrr510@users.noreply.github.com>
2019-11-08 02:59:13 +0530

    Updated members

--- MSC_website/members/urls.py
+++ MSC_website/members/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name = 'home'),
-    path('secretaries/',views.secretaries,name = 'secretaries'),
-    path('core/',views.core,name = 'core'),
-    path('exec/',views.exec,name = 'exec'),
     path('gallery/',views.gallery,name = 'gallery'),
 ]
