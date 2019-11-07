531fb190ea083026811692514c835d30342b88d3
parthrr510 <42448056+parthrr510@users.noreply.github.com>
2019-11-07 16:05:50 +0530

    Revert "Revert "Merge branch 'master' of https://github.com/MicrosoftStudentChapter/MSC-Official-Website""
    This reverts commit 9a2f20603c37533c3f42be1adc252406a927fcd3.

--- MSC_website/members/urls.py
+++ MSC_website/members/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name = 'home'),
-    path('gallery/',views.gallery,name = 'gallery')
+    path('secretaries/',views.secretaries,name = 'secretaries'),
+    path('core/',views.core,name = 'core'),
+    path('exec/',views.exec,name = 'exec'),
+    path('gallery/',views.gallery,name = 'gallery'),
 ]
--- MSC_website/members/views.py
+++ MSC_website/members/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import *
+
 
 # Create your views here.
 
 # for the members page -- /members/
 def home(request):
-    return HttpResponse('You are in Members')
+    return HttpResponse("You are in The Home Page of Members")
+
+def secretaries(request):
+    sec = Secretaries.objects.all()
+    context = {
+        'sec': sec,
+        'header': 'Secretaries',
+    }
+    return render(request,'members/secretaries.html',context)
+def core(request):
+    core = Core.objects.all()
+    context = {
+        'core': core,
+        'header': 'Core',
+    }
+    return render(request,'members/core.html',context)
+
+def exec(request):
+    exec = Executive.objects.all()
+    context = {
+        'exec': exec,
+        'header': 'Executives',
+    }
+    return render(request,'members/exec.html',context)
+
 
 # for the gallery in members -- /members/gallery/
 def gallery(request):
